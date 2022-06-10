8d45b217cb9a0dd43d77372581b2c2b51d0612f9
LEONIDAS <jesus.leon1@unmsm.edu.pe>
2022-06-09 19:37:12 -0500

    feat: add axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const url=`https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=d6f11f82043f67847bb7327a6b572766`;
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      test
     </div>
   );
 }
